33211c833f2fd7a577d31a8e7f9499e60bde2e77
dongtimes2 <98700888+dongtimes2@users.noreply.github.com>
2023-10-04 08:00:00 +0900

    Time: 74 ms (6.99%), Space: 44.4 MB (73.86%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.ts
@@ -0,0 +1,10 @@
+function finalValueAfterOperations(operations: string[]): number {
+  let result = 0;
+  
+  for (const op of operations) {
+    if (op === "--X" || op === "X--") result--;
+    else result++;
+  }
+  
+  return result;
+};
