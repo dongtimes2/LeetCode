bd0fb5872a5b8c852755b505fbbef9bb8d051a9e
dongtimes2 <98700888+dongtimes2@users.noreply.github.com>
2023-10-13 08:00:00 +0900

    Time: 59 ms (36.11%), Space: 44.2 MB (66.48%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.ts
@@ -0,0 +1,15 @@
+function maximumWealth(accounts: number[][]): number {
+  let result = 0;
+  
+  for (let i = 0; i < accounts.length; i++) {
+    let sum = 0;
+    
+    for (let j = 0; j < accounts[0].length; j++) {
+      sum += accounts[i][j];  
+    }
+    
+    result = sum > result ? sum : result;
+  }
+  
+  return result;
+};
