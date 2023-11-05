66a41cf628a08c96fdf07230382de9e95a117126
dongtimes2 <98700888+dongtimes2@users.noreply.github.com>
2023-11-06 08:00:00 +0900

    Time: 75 ms (43.28%), Space: 46.8 MB (56.72%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.ts
@@ -0,0 +1,12 @@
+function decompressRLElist(nums: number[]): number[] {
+  const result = [];
+  
+  for (let i = 0; i < nums.length; i += 2) {
+    const freq = nums[i];
+    const val = nums[i + 1];
+    
+    result.push(...Array(freq).fill(val));
+  }
+  
+  return result;
+};
