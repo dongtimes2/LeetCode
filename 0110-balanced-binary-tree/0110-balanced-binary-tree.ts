4d21610f896ed89f621c0faad4181cf8cfc75619
dongtimes2 <98700888+dongtimes2@users.noreply.github.com>
2023-12-01 11:07:56 +0900

    Time: 61 ms (87.82%), Space: 48.4 MB (35.49%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+  if(!root) return true;
+  
+  let result = true;
+  
+  const find = (node, level) => {
+    if (!node) return 0;
+    
+    const left = find(node.left, level + 1);
+    const right = find(node.right, level + 1);
+    
+   
+    if (Math.abs(right - left) > 1) {
+      result = false;
+    }
+    
+    return Math.max(left, right) + 1;
+  }
+  
+  find(root, 0);
+  
+  return result;
+};
